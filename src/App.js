ce8eeadfc27e39386daab70ce678fb68c2cb3775
JayavelAnandh <jvanandh984@gmail.com>
2023-01-23 22:05:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Base from './Base/base';
+import Studentdetails from './Base/student';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <Base 
+          title = "Batch Details"
+          description ="All student Details">
+            <Studentdetails/>
+          </Base>
     </div>
+    
   );
 }
 
---
+++ src/Base/data.js
@@ -0,0 +1,30 @@
+export const data =[
+    {
+        "id":"1",
+        "name":"Jayavel Anandh",
+        "batch":"B-42-WD",
+        "gender":"Male",
+        "yearsofExperience":"Fresher"
+    },
+    {
+        "id":"2",
+        "name":"Arun Praveen",
+        "batch":"B-42-WD",
+        "gender":"Male",
+        "yearsofExperience":"1"
+    },
+    {
+        "id":"3",
+        "name":"Santhosh Yadav",
+        "batch":"B-42-WD",
+        "gender":"Male",
+        "yearsofExperience":"2"
+    },
+    {
+        "id":"4",
+        "name":"Sharmila",
+        "batch":"B-42-WD",
+        "gender":"Female",
+        "yearsofExperience":"1.5"
+    }
+]
